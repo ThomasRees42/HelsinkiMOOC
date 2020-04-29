e2fb0bebfd875e2ee9cbc68dc7a798c56b01b9e8
Thomas Rees <TSR90909@gmail.com>
2020-04-29 15:11:37 +0100

    Completed Smileys

--- mooc-2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
@@ -6,6 +6,47 @@ public class Smileys {
         // printWithSmileys("Method");
         // printWithSmileys("Beerbottle");
         // printWithSmileys("Interface");
+        printWithSmileys("Mikael");
+        printWithSmileys("\\:D/");
+        printWithSmileys(".87");
+    }
+    
+        private static void printWithSmileys(String characterString) {
+        int length = characterString.length();
+        boolean even = (length % 2 == 0);
+        printLine(length, even);
+        printMiddle(characterString, even);
+        printLine(length,even);
+    }
+    
+    public static void printLine(int length, boolean even) {
+        printSmileys(4);
+        if (even == true) {
+            printSmileys(length);
+        }
+        else {
+            printSmileys(length+1);
+        }
+        printSmileys(2);
+        System.out.print("\n");
+    }
+    
+    public static void printMiddle(String str, boolean even) {
+        printSmileys(2);
+        if (even == true) {
+            System.out.print(" " + str + " ");
+        }
+        else {
+            System.out.print(" " + str + "  ");
+        }
+        printSmileys(2);
+        System.out.print("\n");
+    }
+    
+    public static void printSmileys(int times) {
+        for (int i = 0; i < (times / 2); i++) {
+            System.out.print(":)");
+        }
     }
 
 }
